5234c1e77c1ebc766c0495cf366c4467de392d16
Isidro <irgamez89@gmail.com>
2019-04-01 18:14:14 -0400

    hiding keyboard when editText for title looses focus

--- app/src/main/java/com/dspot/declex/example/todo/ui/addtask/AddTaskFragment.java
+++ app/src/main/java/com/dspot/declex/example/todo/ui/addtask/AddTaskFragment.java
@@ -1,7 +1,10 @@
 package com.dspot.declex.example.todo.ui.addtask;
 
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import com.dspot.declex.example.todo.Navigation;
 import com.dspot.declex.example.todo.R;
@@ -10,6 +13,7 @@ import com.dspot.declex.example.todo.model.TaskToDo;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.FocusChange;
 
 import pl.com.dspot.archiannotations.annotation.ViewModel;
 
@@ -26,4 +30,13 @@ public class AddTaskFragment extends Fragment {
         viewModel.saveTask(new TaskToDo(titleText));
         navigation.goToTaskListFragment();
     }
+
+    @FocusChange
+    public void titleFocusChanged(View view, boolean hasFocus){
+        if (!hasFocus) {
+            InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
 }
