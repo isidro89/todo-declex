e3ce712055ae0422692ab879c77d1ce5e34a3122
Isidro <irgamez89@gmail.com>
2019-04-17 12:14:01 -0400

    bug fix: app crashed when device was rotated after adding a task

--- app/src/main/java/com/dspot/declex/example/todo/ui/addtask/AddTaskViewModel.java
+++ app/src/main/java/com/dspot/declex/example/todo/ui/addtask/AddTaskViewModel.java
@@ -1,10 +1,10 @@
 package com.dspot.declex.example.todo.ui.addtask;
 
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
 import com.dspot.declex.example.todo.DatabaseInstance;
+import com.dspot.declex.example.todo.api.SingleLiveEvent;
 import com.dspot.declex.example.todo.model.TaskToDo;
 
 import org.androidannotations.annotations.Background;
@@ -22,7 +22,7 @@ public class AddTaskViewModel extends ViewModel {
     DatabaseInstance databaseInstance;
 
     @Observable
-    MutableLiveData<Boolean> successfullySaved;
+    SingleLiveEvent<Boolean> successfullySaved;
 
     public LiveData<Boolean> getSuccessfullySaved() {
         return successfullySaved;
