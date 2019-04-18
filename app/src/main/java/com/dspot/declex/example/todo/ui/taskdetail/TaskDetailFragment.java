72c379a6d0c647cb24d63417ef77dc7b8ac11fe9
Isidro <irgamez89@gmail.com>
2019-04-18 16:17:57 -0400

    description textview scrolls for long descriptions and look well in landscape and small screens

--- app/src/main/java/com/dspot/declex/example/todo/ui/taskdetail/TaskDetailFragment.java
+++ app/src/main/java/com/dspot/declex/example/todo/ui/taskdetail/TaskDetailFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -65,6 +66,11 @@ public class TaskDetailFragment extends DialogFragment {
     @ViewById
     TextView taskDescription;
 
+
+    @AfterViews
+    public void setUpViews() {
+        taskDescription.setMovementMethod(new ScrollingMovementMethod());
+    }
 
     @AfterViews
     public void loadTask() {
